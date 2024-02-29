f08b5ca884666aac035d4cc1679faf63b1e564ce
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-02-29 15:53:56 -0600

    test 4 done

--- message.js
+++ message.js
@@ -1,5 +1,16 @@
 class Message {
    // Write code here!
+   // builds an object with two properties
+   constructor(name, commands){
+      // name is a string that is the name of the message
+      // commands is an array of Command objects
+      this.name = name;
+      if (!name) {
+         throw Error("Name required.");
+      }
+      this.commands = commands;
+   }
 }
+
 
 module.exports = Message;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,7 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
-
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Name required.'));
+    })
 });

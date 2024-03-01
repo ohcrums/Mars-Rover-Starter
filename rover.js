a0ef9a0dfbaa95af6b0dc5ece8a8100bd38376a1
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-02-29 20:19:59 -0600

    rover framework

--- rover.js
+++ rover.js
@@ -1,5 +1,17 @@
 class Rover {
    // Write code here!
+
+   // position is a number representing current position
+   constructor(position) {
+      this.position = position;
+      this.mode = 'NORMAL'
+      let generatorWatts = 110;
+   }
+   // function handles updates to rover object properties, takes Message object as argument
+   receiveMessage(message) {
+
+      // returns an object containing two properties: `message`, the name from the original Message; and `results`, an array of results. each element is an object that corresponds to one command in message.commands
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,7 +7,34 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
-
   // 7 tests here!
+  it('constructor sets position and default values for mode and generatorWatts', function() {
+
+  })
+
+  it('response returned by receiveMessage contains the name of the message', function() {
+
+  })
+  
+  it('response returned by receiveMessage includes two results if two commands are sent in the message', function() {
+
+  })
+
+  it('responds correctly to the status check command', function() {
+
+  })
+  
+  it('responds correctly to the mode change command', function() {
+
+  })
+  
+  it('responds with a false completed value when attempting to move in LOW_POWER mode', function() {
+
+  })
+  
+  it('responds with the position for the move command', function() {
+
+  })
+  
 
 });

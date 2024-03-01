c635ef9864c34b26046c79e92875bff95929737f
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-02-29 22:16:06 -0600

    improve test structure

--- rover.js
+++ rover.js
@@ -4,13 +4,20 @@ class Rover {
    // position is a number representing current position
    constructor(position) {
       this.position = position;
+      if (!position) {
+         throw Error("Position required.");
+      }
       this.mode = 'NORMAL'
-      let generatorWatts = 110;
+      this.generatorWatts = 110;
    }
    // function handles updates to rover object properties, takes Message object as argument
    receiveMessage(message) {
-
-      // returns an object containing two properties: `message`, the name from the original Message; and `results`, an array of results. each element is an object that corresponds to one command in message.commands
+        // returns an object containing two properties: `message`, the name from the original Message; and `results`, an array of results. each element is an object that corresponds to one command in message.commands
+      let response = {
+         message: message.name,
+         results: message.commands
+      };
+      return response;
    }
 }
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,34 +7,49 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
-  // 7 tests here!
-  it('constructor sets position and default values for mode and generatorWatts', function() {
+  let testRover = new Rover(1234);
+  describe("basic object checks", function() {
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message ('i am a name', commands);
+    let response = testRover.receiveMessage(message);
 
+    it('constructor sets position and default values for mode and generatorWatts', function() {
+      expect(testRover.position).toBe(1234);
+      expect(testRover.mode).toBe('NORMAL');
+      expect(testRover.generatorWatts).toBe(110)
+    })
+
+    it('response returned by receiveMessage contains the name of the message', function() {
+      expect(response.message).toContain(message.name);
+    })
+
+    it('response returned by receiveMessage includes two results if two commands are sent in the message', function() {
+      expect(response.results.length).toBe(2);
+    })
   })
 
-  it('response returned by receiveMessage contains the name of the message', function() {
-
-  })
+  describe("Command checks", function() {
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let message = new Message ('i am a name', commands);
+    let response = testRover.receiveMessage(message);
+    
+    it('responds correctly to the status check command', function() {
+    
+    })
+    
+    it('responds correctly to the mode change command', function() {
   
-  it('response returned by receiveMessage includes two results if two commands are sent in the message', function() {
-
-  })
-
-  it('responds correctly to the status check command', function() {
-
-  })
+    })
+    
+    it('responds with a false completed value when attempting to move in LOW_POWER mode', function() {
   
-  it('responds correctly to the mode change command', function() {
-
-  })
+    })
+    
+    it('responds with the position for the move command', function() {
   
-  it('responds with a false completed value when attempting to move in LOW_POWER mode', function() {
-
+    })
+    
   })
-  
-  it('responds with the position for the move command', function() {
 
-  })
-  
 
 });
